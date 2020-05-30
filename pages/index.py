9a2561f4f80ea7881f11071891633ca246e1945f
Yoni Pineda <yonipineda1010@icloud.com>
2020-05-30 14:58:00 -0400

    Basic template for dash app.

---
+++ pages/index.py
@@ -0,0 +1,45 @@
+# Imports from 3rd party libraries
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Imports from this application
+from app import app
+
+# 2 column layout. 1st column width = 4/12
+# https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+
+            #### Montgomery County Crime Prediction
+
+            Born out of a curiosity to know how my intuition matched with the data in regards to crime levels in my childhood city and state.
+            Montgomery Crime Predictor is trained on a RandomForest Model with ~200,000 observations. Using featuers such as
+            Time, location, and place, this model attempts to predict the tipe of crime that will occur. This was a project
+            made for learning and as a Unit Assesment for Unit 2 at [lambda School](https://lambdaschool.com/). Try it out
+            by clicking on the button below!
+
+            """
+        ),
+        dcc.Link(dbc.Button('Predict The Crime',
+                            color='primary'), href='/predictions')
+    ],
+    md=4,
+)
+
+gapminder = px.data.gapminder()
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+                 hover_name="country", log_x=True, size_max=60)
+
+column2 = dbc.Col(
+    [
+        dcc.Graph(figure=fig),
+    ]
+)
+
+layout = dbc.Row([column1, column2])
