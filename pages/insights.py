21a004ddb5170068e49c686e221262d7d7e37888
Yoni Pineda <yonipineda1010@icloud.com>
2020-05-31 20:49:43 -0400

    insights page done. Process developing.

--- pages/insights.py
+++ pages/insights.py
@@ -12,16 +12,59 @@ from app import app
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
-        dcc.Markdown(
-            """
-        
-            ## Insights
+        html.H2("Insights"),
 
+        dcc.Markdown(
+
+            """
+
+            I went into this project with biased predictions due to years of experience
+            in these parts. I knew some parts were known to be a bit more risky, whether
+            it was true or not, possibly just talk. I had already built images of these
+            different places in Montgomery County. It is generally known that at night
+            time, risk increases: this was true in this case. It seems Time -- the hour
+            of the day, month of the year, and even minute of the hour can predict if
+            a crime will occur in a given street. Location was another one, it seems
+            certain Zip Code were I predictor. Now, I do want to stress that this in no
+            way should be taken seriously, this is the work of a student with no more than
+            a couple of months with programming experience and just enough understanding
+            of statistics to be able to do just to data. Below is a picture illustrating
+            the Feature Importance for Street Name Prediction:
 
             """
         ),
+        html.Img(src='assets/feature_importance.png', className='img-fluid'),
+        html.Br(),
+        html.Div([
+
+            html.Br(),
+            html.P(
+                """
+                    This next illustration, is a eli5 Permutation Feature Importance Graph.
+                    Shows sector and Zip Code holding the majority of the weight.
+
+                    """
+            ),
+            html.Img(src='assets/permutation.png', className='img-fluid')
+
+        ]),
+        html.Br(),
+        html.Div([
+
+            dcc.Markdown(
+                '''
+            In this gif animation below, you can see a [Partial Dependece Plot](https://blogs.sas.com/content/subconsciousmusings/2018/06/12/interpret-model-predictions-with-partial-dependence-and-individual-conditional-expectation-plots/).
+            The line represents change in prediction based on adjustment of the features value. It is a PDP
+            of Zip Code and Hour in relation to Street Name.
+
+            ![header](/assets/PDP.gif)
+
+            '''
+            ),
+        ])
 
     ],
+    md=10
 )
 
 layout = dbc.Row([column1])
--- pages/process.py
+++ pages/process.py
@@ -12,10 +12,23 @@ from app import app
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
+        html.H2("Process: Messy data, Data Wrangling -> Model Training."),
         dcc.Markdown(
             """
-        
-            ## Process
+
+            Trying to find a solution yet alone a problem to address with any dataset
+            is challenging in of itself. In some cases, it requires some domain knowledge
+            of the data you are inspecting. Data is also very messy, chaos insues in data.
+            There can be order, and a pattern can be found which can lead to insightful findings.
+            Lets take a look at how the data came in as I started:
+
+            ![messy](/assets/messy.gif) 
+
+            There were a number of missing values, parsing the data was a challenge and, when it came
+            to structuring it for training, much more needed to be done.
+
+
+
 
 
             """
